6e6ee5287225d4cccd766798003732310913a49b
ward <me@ward.party>
2026-08-26 12:44:46 +0100

    ref: added comments

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
 export const config = {
+  // discord
   token: process.env.DISCORD_TOKEN!,
   prefix: process.env.PREFIX ?? ",",
 
@@ -7,4 +8,6 @@ export const config = {
   supabase_secret_key: process.env.SUPABASE_SECRET_KEY!,
   supabase_public_key: process.env.SUPABASE_PUBLISHABLE_KEY!,
   supabase_jwks_url: process.env.SUPABASE_JWKS_URL!,
+
+  // API keys
 };
